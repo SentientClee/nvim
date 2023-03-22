6475ccf79fc53dd45a611ff41e236f28f76f1b8a
Chris <chris.r.lee43@gmail.com>
2023-03-21 17:09:41 -0700

    add eslint_d as diagnostic

--- lua/lsp/format.lua
+++ lua/lsp/format.lua
@@ -32,6 +32,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.diagnostics.eslint_d,
 	},
 	on_attach = on_attach,
 })
