3362283a7c5327e9ee905e54d3ec498d279640aa
Chris <chris.r.lee43@gmail.com>
2023-04-20 19:50:32 -0700

    Add svelte filetype support to prettierd

--- lua/lsp/format.lua
+++ lua/lsp/format.lua
@@ -31,7 +31,9 @@ end
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prettierd.with({
+			extra_filetypes = { "svelte" },
+		}),
 		null_ls.builtins.diagnostics.eslint_d.with({
 			condition = function(utils)
 				return utils.root_has_file({ ".eslintrc.json" })
