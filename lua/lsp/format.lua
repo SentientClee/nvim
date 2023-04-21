7c7d5a5ab0a091d6346022b665cf83703b97a55a
Chris <chris.r.lee43@gmail.com>
2023-04-20 19:32:38 -0700

    only eslint when config present

--- lua/lsp/format.lua
+++ lua/lsp/format.lua
@@ -32,7 +32,11 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.json" })
+			end,
+		}),
 	},
 	on_attach = on_attach,
 })
