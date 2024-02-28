26ee8e212eb6becd12f723531b23c8b31f328373
SentientClee <clee@goguardian.com>
2024-02-27 20:04:59 -0800

    Added more plugins

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Required mainly for [fzf-lua](https://github.com/ibhagwan/fzf-lua). These can be
 ## Notes
 
 A list of things I want to do better in Neovim and so should configure.
+
+- README.md preview ability
+- Better text object mappings for tree sitter for improved movement
+- Auto pairs for closing (), {}, []. Likely requires integration with tree sitter.
+- Better keymaps for Gitsigns and git work
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,23 +1,37 @@
 return {
-	"kyazdani42/nvim-web-devicons",
-	"folke/neodev.nvim", -- provides extra lua lsp configs for vim configging
+  {
+    -- lua functions
+    "nvim-lua/plenary.nvim", 
+  },
+  {
+    -- icons
+    "kyazdani42/nvim-web-devicons"
+  },
+  {
+    -- provides extra lua lsp configs for vim configging
+    "folke/neodev.nvim",
+  },
 	{
-		"folke/which-key.nvim", -- popup that shows commands
+    -- popup ui with command aids
+		"folke/which-key.nvim",
 		event = "VeryLazy",
+    config = function()
+      require("which-key").setup()
+    end
 	},
 	{
+    -- colorscheme
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			-- load the colorscheme here
 			vim.cmd([[colorscheme tokyonight]])
 		end,
 	},
   {
+    -- netrw style file/folder navigation shortcuts
     'stevearc/oil.nvim',
-    -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("oil").setup({
@@ -28,5 +42,62 @@ return {
 				},
 			})
 		end,
-  }
+  },
+  {
+    -- Highlights
+    -- TODO
+    -- NOTE
+    -- WARNING
+    -- FIX
+		"folke/todo-comments.nvim",
+		config = function()
+			require("todo-comments").setup({
+				highlight = {
+					pattern = [[.*<(KEYWORDS)\s*]],
+				},
+				search = {
+					pattern = [[\b(KEYWORDS)\b]],
+				},
+			})
+		end,
+	},
+  {
+    -- statusline
+		"nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+				},
+			})
+		end,
+    dependencies = {
+      {
+        "nvim-tree/nvim-web-devicons",
+        lazy = true,
+      },
+    }
+	},
+  {
+		"alvarosevilla95/luatab.nvim",
+		config = function()
+			require("luatab").setup({})
+		end,
+	},
+  {
+    -- fuzzy finder
+    -- NOTE: Be sure to install 'fd' and 'rg' for find and fuzzy search.
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    -- Git signs in gutter
+		"lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup()
+		end,
+	}
 }
