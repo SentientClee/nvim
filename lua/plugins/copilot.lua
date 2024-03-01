7e1b2fefc307703a8f3fa0cb552c717585992b2b
SentientClee <clee@goguardian.com>
2024-02-29 21:14:42 -0800

    Add copilot configuration

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  build = ":Copilot auth",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+      },
+    })
+  end,
+}
