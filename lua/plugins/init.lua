310fc033da9a8c00172af1c109bf94fff7691b92
SentientClee <clee@goguardian.com>
2024-06-27 11:28:25 -0700

    Add go to implementation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -135,4 +135,12 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
