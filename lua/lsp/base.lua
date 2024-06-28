a3d07409adeecece6a4597c00b932ab333c3fe12
SentientClee <clee@goguardian.com>
2024-06-28 15:13:55 -0700

    More lsp cleanup

--- lua/lsp/base.lua
+++ lua/lsp/base.lua
@@ -3,14 +3,19 @@ local base = {}
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 base.on_attach = function(_, bufnr)
-  local keymap = vim.keymap.set
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  local function map(m, k, v, opts)
+    opts = opts or {}
+    opts.silent = true
+    opts.noremap = true
+    opts.buffer = bufnr
+    vim.keymap.set(m, k, v, opts)
+  end
 
   -- Go to declaration
-  keymap("n", "gD", vim.lsp.buf.declaration, bufopts)
+  map("n", "gD", vim.lsp.buf.declaration)
 
   -- Go to implementation
-  keymap("n", "gi", vim.lsp.buf.implementation, bufopts)
+  map("n", "gi", vim.lsp.buf.implementation)
 
   -- Go to definition or peek definition
   -- You can edit the file containing the definition in the floating window
@@ -20,7 +25,7 @@ base.on_attach = function(_, bufnr)
   -- keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
   -- Go to definition
   -- keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
-  keymap("n", "gd", vim.lsp.buf.definition, bufopts)
+  map("n", "gd", vim.lsp.buf.definition)
 
   -- Peek type definition
   -- You can edit the file containing the type definition in the floating window
@@ -30,11 +35,11 @@ base.on_attach = function(_, bufnr)
   -- keymap("n", "gy", "<cmd>Lspsaga peek_type_definition<CR>")
   -- Go to type definition
   -- keymap("n", "gy", "<cmd>Lspsaga goto_type_definition<CR>")
-  keymap("n", "gy", vim.lsp.buf.type_definition, bufopts)
+  map("n", "gy", vim.lsp.buf.type_definition)
 
   -- References
-  keymap("n", "gr", "<cmd>FzfLua lsp_references<CR>")
-  -- keymap('n', 'gr', vim.lsp.buf.references, bufopts)
+  map("n", "gr", "<cmd>FzfLua lsp_references<CR>")
+  -- keymap('n', 'gr', vim.lsp.buf.references)
 
   -- Hover Doc
   -- If there is no hover doc,
@@ -49,31 +54,31 @@ base.on_attach = function(_, bufnr)
   -- Note that if you use hover with ++keep, pressing this key again will
   -- close the hover window. If you want to jump to the hover window
   -- you should use the wincmd command "<C-w>w"
-  keymap("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
-  -- keymap('n', 'K', vim.lsp.buf.hover, bufopts)
+  map("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+  -- keymap('n', 'K', vim.lsp.buf.hover)
 
   -- Toggle outline
-  keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+  map("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
 
   -- Call hierarchy
-  keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
-  keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+  map("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+  map("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 
   -- Format buffer
-  -- keymap('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  -- keymap('n', '<space>f', function() vim.lsp.buf.format { async = true } end)
 
   -- Code action
-  keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-  -- keymap('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+  -- keymap('n', '<space>ca', vim.lsp.buf.code_action)
 
   -- Rename all occurrences of the hovered word for the entire file
-  keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
-  -- keymap('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
+  -- keymap('n', '<space>rn', vim.lsp.buf.rename)
 
   -- Show buffer diagnostics
-  keymap("n", "<leader>sb", vim.diagnostic.setloclist, bufopts)
+  map("n", "<leader>sb", vim.diagnostic.setloclist)
   -- Show workspace diagnostics
-  keymap("n", "<leader>sw", vim.diagnostic.setqflist, bufopts)
+  map("n", "<leader>sw", vim.diagnostic.setqflist)
 
   -- Show line diagnostics
   -- You can pass argument ++unfocus to
@@ -85,23 +90,23 @@ base.on_attach = function(_, bufnr)
   -- keymap("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
 
   -- Diagnostic jump
-  -- keymap('n', '<space>e', vim.diagnostic.open_float, bufopts)
+  -- keymap('n', '<space>e', vim.diagnostic.open_float)
   -- You can use <C-o> to jump back to your previous location
-  keymap("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-  -- keymap('n', '[d', vim.diagnostic.goto_prev, bufopts)
-  keymap("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
-  -- keymap('n', ']d', vim.diagnostic.goto_next, bufopts)
+  map("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+  -- keymap('n', '[d', vim.diagnostic.goto_prev)
+  map("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+  -- keymap('n', ']d', vim.diagnostic.goto_next)
 
   -- Diagnostic jump with filters such as only jumping to an error
-  keymap("n", "[G", function()
+  map("n", "[G", function()
     require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
   end)
-  keymap("n", "]G", function()
+  map("n", "]G", function()
     require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
   end)
 
   -- Floating terminal
-  keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+  map({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
 end
 
 return base
