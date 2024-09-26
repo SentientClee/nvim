2c85d4f6cb69fd4af452290a59527c53e4f2fddc
SentientClee <clee@goguardian.com>
2024-09-25 18:42:50 -0700

    Flow support

--- lua/lsp/lang.lua
+++ lua/lsp/lang.lua
@@ -2,7 +2,28 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = require("lsp.base").on_attach
 local lspconfig = require("lspconfig")
 
+-- Typescript
 lspconfig["ts_ls"].setup({
+  on_attach = function(client, bufnr)
+    if vim.fn.filereadable(client.config.root_dir .. "/.flowconfig") == 1 then
+      -- Turn off the ts_ls client if flow is present
+      client.stop()
+      return
+    else
+      on_attach(client, bufnr)
+    end
+  end,
+  capabilities = capabilities,
+  on_new_config = function(config, root_dir)
+    if vim.fn.filereadable(root_dir .. "/.flowconfig") == 1 then
+      -- Turn off single file support if flow is present
+      config.single_file_support = false
+    end
+  end,
+})
+
+-- Flow type
+lspconfig["flow"].setup({
   on_attach = on_attach,
   capabilities = capabilities,
 })
